3514cb7bc2068c3a57b9f4395b398b48a3a0b9b3
xvicarious <bmaurer@xvicario.us>
2017-03-22 14:02:06 -0400

    added card id

--- www/php/new_deck.php
+++ www/php/new_deck.php
@@ -4,17 +4,17 @@ require "mPDO.php";
 include "SqlStatements.php";
 //if (sessionCheck()) {
     $deckJSON = $_POST['deck'];
-    $deckUser = 1; //$_POST['userId'];
-    $deckTournament = 0; //$_POST['tournament']; // todo: add tournament database
+    //$deckUser = $_POST['userId'];
+    //$deckTournament = $_POST['tournament']; // todo: add tournament database
     $deckDate = date('Y-m-d H:i:s'); // when this deck was made
-    $dbh = createPDO();
+    $dbh = createPDO2();
     try {
-        // [cardQuantity, cardName, sideboard]
+        // [cardId, cardQuantity, cardName, sideboard]
         $deckObject = json_decode($deckJSON);
         $zero = 0;
         // First, create the deck
         $stmt = $dbh->prepare(SqlStatements::INSERT_NEW_DECK);
-        $stmt->bindParam(':userId', $deckUser, PDO::PARAM_INT);
+        $stmt->bindParam(':userId', $zero, PDO::PARAM_INT);
         // todo: support for tournaments, formats, and archetypes
         $stmt->bindParam(':tournamentId', $zero, PDO::PARAM_INT);
         $stmt->bindParam(':formatId', $zero, PDO::PARAM_INT);
@@ -22,18 +22,15 @@ include "SqlStatements.php";
         $stmt->execute();
         $last_id = $dbh->lastInsertId();
         foreach ($deckObject as $card) {
-            // Make sure the card is in the database, seems silly, but this is
-            // the best way I know how
-            $stmt = $dbh->prepare(SqlStatements::INSERT_CARD);
-            $stmt->bindParam(':cardName', $card[1], PDO::PARAM_STR);
-            $stmt->execute();
+            echo $card[0],$card[1],$card[2],$card[3];
             // Now put the card into the deck
             $stmt = $dbh->prepare(SqlStatements::INSERT_CARD_INTO_DECK);
-            $stmt->bindParam(':cardName', $card[1], PDO::PARAM_STR);
+            $stmt->bindParam(':cardId', $card[0], PDO::PARAM_INT);
             $stmt->bindParam(':deckId', $last_id, PDO::PARAM_INT);
-            $stmt->bindParam(':cardQuantity', $card[0], PDO::PARAM_INT);
-            $stmt->bindParam(':isSideboard', $card[2], PDO::PARAM_BOOL);
+            $stmt->bindParam(':cardQuantity', $card[1], PDO::PARAM_INT);
+            $stmt->bindParam(':isSideboard', $card[3], PDO::PARAM_BOOL);
             $stmt->execute();
+            $stmt->debugDumpParams();
         }
     } catch (PDOException $e) {
         error_log($e->getMessage(), 0);
